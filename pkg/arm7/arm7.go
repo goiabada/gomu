8ca6c0e1ea8d182ce55beee5f2f69d9745f5ccd5
Nikolas Vago Serafini <nikolas.serafini@zrp.com.br>
2019-08-06 14:29:00 -0300

    Refactored ARM7 structs, fixed and added a couple of tests

--- pkg/arm7/arm7.go
+++ pkg/arm7/arm7.go
@@ -14,58 +14,58 @@ type CPU struct {
 // RegisterSet defines virtual CPU registers
 type RegisterSet struct {
 	// General Purpose Registers
-	r0  uint32
-	r1  uint32
-	r2  uint32
-	r3  uint32
-	r4  uint32
-	r5  uint32
-	r6  uint32
-	r7  uint32
-	r8  uint32
-	r9  uint32
-	r10 uint32
-	r11 uint32
-	r12 uint32
+	R0  uint32
+	R1  uint32
+	R2  uint32
+	R3  uint32
+	R4  uint32
+	R5  uint32
+	R6  uint32
+	R7  uint32
+	R8  uint32
+	R9  uint32
+	R10 uint32
+	R11 uint32
+	R12 uint32
 	// Stack Pointer - SP
-	r13 uint32
+	R13 uint32
 	// Link Register - LP
-	r14 uint32
+	R14 uint32
 	// Program Counter - PC
-	r15 uint32
+	R15 uint32
 	// Current Program Status Register - CPSR
-	cpsr uint32
+	Cpsr uint32
 
 	// Banked Registers for Fast Interrupt Request (FIQ) mode
-	r8FIQMode  uint32
-	r9FIQMode  uint32
-	r10FIQMode uint32
-	r11FIQMode uint32
-	r12FIQMode uint32
-	r13FIQMode uint32
-	r14FIQMode uint32
+	R8FIQMode  uint32
+	R9FIQMode  uint32
+	R10FIQMode uint32
+	R11FIQMode uint32
+	R12FIQMode uint32
+	R13FIQMode uint32
+	R14FIQMode uint32
 	// Saved Program Status Register - SPSR
-	spsrFiq uint32
+	SpsrFiq uint32
 
 	// Banked Supervisor Calls (SVC) mode registers
-	r13SupervisorMode  uint32
-	r14SupervisorMode  uint32
-	spsrSupervisorMode uint32
+	R13SupervisorMode  uint32
+	R14SupervisorMode  uint32
+	SpsrSupervisorMode uint32
 
 	// Banked Abort Mode (ABT) registers
-	r13AbortMode  uint32
-	r14AbortMode  uint32
-	spsrAbortMode uint32
+	R13AbortMode  uint32
+	R14AbortMode  uint32
+	SpsrAbortMode uint32
 
 	// Banked Interrupt Mode (IRQ) registers
-	r13InterruptMode  uint32
-	r14InterruptMode  uint32
-	spsrInterruptMode uint32
+	R13InterruptMode  uint32
+	R14InterruptMode  uint32
+	SpsrInterruptMode uint32
 
 	// Banked Undefined Mode registers
-	r13UndefinedMode  uint32
-	r14UndefinedMode  uint32
-	spsrUndefinedMode uint32
+	R13UndefinedMode  uint32
+	R14UndefinedMode  uint32
+	SpsrUndefinedMode uint32
 }
 
 // Constants for defining the CPU modes
@@ -86,31 +86,31 @@ const (
 )
 
 func (registers *RegisterSet) reset(usingBIOS bool) {
-	registers.r0, registers.r1, registers.r2, registers.r3, registers.r4, registers.r5 = 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
-	registers.r6, registers.r7, registers.r8, registers.r9, registers.r10 = 0x0, 0x0, 0x0, 0x0, 0x0
-	registers.r11, registers.r12, registers.r14 = 0x0, 0x0, 0x0
+	registers.R0, registers.R1, registers.R2, registers.R3, registers.R4, registers.R5 = 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
+	registers.R6, registers.R7, registers.R8, registers.R9, registers.R10 = 0x0, 0x0, 0x0, 0x0, 0x0
+	registers.R11, registers.R12, registers.R14 = 0x0, 0x0, 0x0
 
-	registers.r8FIQMode, registers.r9FIQMode, registers.r10FIQMode, registers.r11FIQMode = 0x0, 0x0, 0x0, 0x0
-	registers.r12FIQMode, registers.r14FIQMode = 0x0, 0x0
+	registers.R8FIQMode, registers.R9FIQMode, registers.R10FIQMode, registers.R11FIQMode = 0x0, 0x0, 0x0, 0x0
+	registers.R12FIQMode, registers.R14FIQMode = 0x0, 0x0
 
-	registers.r14SupervisorMode, registers.spsrSupervisorMode = 0x0, 0x0
-	registers.r14AbortMode, registers.spsrAbortMode = 0x0, 0x0
-	registers.r14InterruptMode, registers.spsrInterruptMode = 0x0, 0x0
-	registers.r14UndefinedMode, registers.spsrUndefinedMode = 0x0, 0x0
+	registers.R14SupervisorMode, registers.SpsrSupervisorMode = 0x0, 0x0
+	registers.R14AbortMode, registers.SpsrAbortMode = 0x0, 0x0
+	registers.R14InterruptMode, registers.SpsrInterruptMode = 0x0, 0x0
+	registers.R14UndefinedMode, registers.SpsrUndefinedMode = 0x0, 0x0
 
 	// If not booting from the BIOS
 	if !usingBIOS {
-		registers.r13, registers.r13FIQMode, registers.r13AbortMode, registers.r13UndefinedMode = 0x03007F00, 0x03007F00, 0x03007F00, 0x03007F00
-		registers.r15 = 0x8000000
-		registers.r13SupervisorMode = 0x03007FE0
-		registers.r13InterruptMode = 0x03007FA0
-		registers.cpsr = 0x5F
+		registers.R13, registers.R13FIQMode, registers.R13AbortMode, registers.R13UndefinedMode = 0x03007F00, 0x03007F00, 0x03007F00, 0x03007F00
+		registers.R15 = 0x8000000
+		registers.R13SupervisorMode = 0x03007FE0
+		registers.R13InterruptMode = 0x03007FA0
+		registers.Cpsr = 0x5F
 	} else {
-		registers.r13, registers.r13FIQMode, registers.r13AbortMode, registers.r13UndefinedMode = 0x0, 0x0, 0x0, 0x0
-		registers.r15 = 0x0
-		registers.r13SupervisorMode = 0x0
-		registers.r13InterruptMode = 0x0
-		registers.cpsr = 0xD3
+		registers.R13, registers.R13FIQMode, registers.R13AbortMode, registers.R13UndefinedMode = 0x0, 0x0, 0x0, 0x0
+		registers.R15 = 0x0
+		registers.R13SupervisorMode = 0x0
+		registers.R13InterruptMode = 0x0
+		registers.Cpsr = 0xD3
 	}
 
 }
@@ -118,78 +118,78 @@ func (registers *RegisterSet) reset(usingBIOS bool) {
 func (cpu *CPU) getRegister(register uint32) uint32 {
 	switch register {
 	case 0:
-		return cpu.registers.r0
+		return cpu.registers.R0
 	case 1:
-		return cpu.registers.r1
+		return cpu.registers.R1
 	case 2:
-		return cpu.registers.r2
+		return cpu.registers.R2
 	case 3:
-		return cpu.registers.r3
+		return cpu.registers.R3
 	case 4:
-		return cpu.registers.r4
+		return cpu.registers.R4
 	case 5:
-		return cpu.registers.r5
+		return cpu.registers.R5
 	case 6:
-		return cpu.registers.r6
+		return cpu.registers.R6
 	case 7:
-		return cpu.registers.r7
+		return cpu.registers.R7
 	case 8:
 		if cpu.cpuMode == FIQ {
-			return cpu.registers.r8FIQMode
+			return cpu.registers.R8FIQMode
 		}
-		return cpu.registers.r8
+		return cpu.registers.R8
 	case 9:
 		if cpu.cpuMode == FIQ {
-			return cpu.registers.r9FIQMode
+			return cpu.registers.R9FIQMode
 		}
-		return cpu.registers.r9
+		return cpu.registers.R9
 	case 10:
 		if cpu.cpuMode == FIQ {
-			return cpu.registers.r10FIQMode
+			return cpu.registers.R10FIQMode
 		}
-		return cpu.registers.r10
+		return cpu.registers.R10
 	case 11:
 		if cpu.cpuMode == FIQ {
-			return cpu.registers.r11FIQMode
+			return cpu.registers.R11FIQMode
 		}
-		return cpu.registers.r11
+		return cpu.registers.R11
 	case 12:
 		if cpu.cpuMode == FIQ {
-			return cpu.registers.r12FIQMode
+			return cpu.registers.R12FIQMode
 		}
-		return cpu.registers.r12
+		return cpu.registers.R12
 	case 13:
 		switch cpu.cpuMode {
 		case SYS:
-			return cpu.registers.r13
+			return cpu.registers.R13
 		case FIQ:
-			return cpu.registers.r13FIQMode
+			return cpu.registers.R13FIQMode
 		case SVC:
-			return cpu.registers.r13SupervisorMode
+			return cpu.registers.R13SupervisorMode
 		case ABT:
-			return cpu.registers.r13AbortMode
+			return cpu.registers.R13AbortMode
 		case IRQ:
-			return cpu.registers.r13InterruptMode
+			return cpu.registers.R13InterruptMode
 		case UND:
-			return cpu.registers.r13UndefinedMode
+			return cpu.registers.R13UndefinedMode
 		}
 	case 14:
 		switch cpu.cpuMode {
 		case SYS:
-			return cpu.registers.r14
+			return cpu.registers.R14
 		case FIQ:
-			return cpu.registers.r14FIQMode
+			return cpu.registers.R14FIQMode
 		case SVC:
-			return cpu.registers.r14SupervisorMode
+			return cpu.registers.R14SupervisorMode
 		case ABT:
-			return cpu.registers.r14AbortMode
+			return cpu.registers.R14AbortMode
 		case IRQ:
-			return cpu.registers.r14InterruptMode
+			return cpu.registers.R14InterruptMode
 		case UND:
-			return cpu.registers.r14UndefinedMode
+			return cpu.registers.R14UndefinedMode
 		}
 	case 15:
-		return cpu.registers.r15
+		return cpu.registers.R15
 	default:
 		log.Println("You're are trying to access a non-existent register!")
 	}
@@ -200,90 +200,90 @@ func (cpu *CPU) getRegister(register uint32) uint32 {
 func (cpu *CPU) setRegister(register uint32, value uint32) {
 	switch register {
 	case 0:
-		cpu.registers.r0 = value
+		cpu.registers.R0 = value
 	case 1:
-		cpu.registers.r1 = value
+		cpu.registers.R1 = value
 	case 2:
-		cpu.registers.r2 = value
+		cpu.registers.R2 = value
 	case 3:
-		cpu.registers.r3 = value
+		cpu.registers.R3 = value
 	case 4:
-		cpu.registers.r4 = value
+		cpu.registers.R4 = value
 	case 5:
-		cpu.registers.r5 = value
+		cpu.registers.R5 = value
 	case 6:
-		cpu.registers.r6 = value
+		cpu.registers.R6 = value
 	case 7:
-		cpu.registers.r7 = value
+		cpu.registers.R7 = value
 	case 8:
 		if cpu.cpuMode == FIQ {
-			cpu.registers.r8FIQMode = value
+			cpu.registers.R8FIQMode = value
 		} else {
-			cpu.registers.r8 = value
+			cpu.registers.R8 = value
 		}
 	case 9:
 		if cpu.cpuMode == FIQ {
-			cpu.registers.r9FIQMode = value
+			cpu.registers.R9FIQMode = value
 		} else {
-			cpu.registers.r9 = value
+			cpu.registers.R9 = value
 		}
 	case 10:
 		if cpu.cpuMode == FIQ {
-			cpu.registers.r10FIQMode = value
+			cpu.registers.R10FIQMode = value
 		} else {
-			cpu.registers.r10 = value
+			cpu.registers.R10 = value
 		}
 	case 11:
 		if cpu.cpuMode == FIQ {
-			cpu.registers.r11FIQMode = value
+			cpu.registers.R11FIQMode = value
 		} else {
-			cpu.registers.r11 = value
+			cpu.registers.R11 = value
 		}
 	case 12:
 		if cpu.cpuMode == FIQ {
-			cpu.registers.r12FIQMode = value
+			cpu.registers.R12FIQMode = value
 		} else {
-			cpu.registers.r12 = value
+			cpu.registers.R12 = value
 		}
 	case 13:
 		switch cpu.cpuMode {
 		case SYS:
-			cpu.registers.r13 = value
+			cpu.registers.R13 = value
 		case FIQ:
-			cpu.registers.r13FIQMode = value
+			cpu.registers.R13FIQMode = value
 		case SVC:
-			cpu.registers.r13SupervisorMode = value
+			cpu.registers.R13SupervisorMode = value
 		case ABT:
-			cpu.registers.r13AbortMode = value
+			cpu.registers.R13AbortMode = value
 		case IRQ:
-			cpu.registers.r13InterruptMode = value
+			cpu.registers.R13InterruptMode = value
 		case UND:
-			cpu.registers.r13UndefinedMode = value
+			cpu.registers.R13UndefinedMode = value
 		}
 	case 14:
 		switch cpu.cpuMode {
 		case SYS:
-			cpu.registers.r14 = value
+			cpu.registers.R14 = value
 		case FIQ:
-			cpu.registers.r14FIQMode = value
+			cpu.registers.R14FIQMode = value
 		case SVC:
-			cpu.registers.r14SupervisorMode = value
+			cpu.registers.R14SupervisorMode = value
 		case ABT:
-			cpu.registers.r14AbortMode = value
+			cpu.registers.R14AbortMode = value
 		case IRQ:
-			cpu.registers.r14InterruptMode = value
+			cpu.registers.R14InterruptMode = value
 		case UND:
-			cpu.registers.r14UndefinedMode = value
+			cpu.registers.R14UndefinedMode = value
 		}
 	case 15:
-		cpu.registers.r15 = value
+		cpu.registers.R15 = value
 	default:
 		log.Println("You're are trying to overwrite a non-existent register!")
 	}
 }
 
 // BranchWithLink executes correspondent CPU instruction
-func BranchWithLink(instruction []byte, registers *RegisterSet) {
+func (cpu *CPU) BranchWithLink(instruction []byte) {
 	// First, we correct the byte order of the opcode
 	fixedOInstruction := translateLittleEndianInstruction(instruction)
 
@@ -298,34 +298,33 @@ func BranchWithLink(instruction []byte, registers *RegisterSet) {
 	// For the opcode, we shift right 24 bits and grab just the first bit
 	opcode := (fixedOInstruction >> 24) & 0x1
 	/// Update the program counter
-	registers.r15 += 8 + 4*offset
+	cpu.setRegister(15, cpu.getRegister(15)+8+4*offset)
 
 	switch opcode {
 	// Branch operation
 	case 0x0:
 	// Branch with link operation
 	case 0x1:
-		registers.r14 = registers.r15 + 4
+		cpu.setRegister(14, cpu.getRegister(15)+4)
 	}
 
 }
 
 // BranchAndExchange executes correspondent CPU instruction
-func BranchAndExchange(instruction []byte, registers *RegisterSet) {
+func (cpu *CPU) BranchAndExchange(instruction []byte) {
 	fixedInstruction := translateLittleEndianInstruction(instruction)
 
 	sourceRegister := fixedInstruction & 0xF
 	operation := (fixedInstruction >> 4) & 0x1
 
-	actualRegisterValue := registers.getRegister(sourceRegister)
-	registers.r15 = actualRegisterValue
+	actualRegisterValue := cpu.getRegister(sourceRegister)
+	cpu.registers.R15 = actualRegisterValue
 
 	switch operation {
 	// Branch
 	case 0:
 	// Branch and Exchange
 	case 3:
-		registers.r14 = registers.r15 + 0x4
+		cpu.setRegister(14, cpu.getRegister(15)+0x4)
 	}
-
 }
--- pkg/arm7/arm7_test.go
+++ pkg/arm7/arm7_test.go
@@ -2,22 +2,23 @@ package arm7
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
 type Arm7TestSuite struct {
 	suite.Suite
-	registers registerSet
+	cpu CPU
 }
 
 func (suite *Arm7TestSuite) SetupTest() {
-	suite.registers.r0 = 0x5
-	suite.registers.r13 = 0x15
-	suite.registers.r15 = 0x20
-	suite.registers.r13SupervisorMode = 0x25
-	suite.registers.r13InterruptMode = 0x30
-	suite.registers.cpsr = 0x35
+	suite.cpu.registers.R0 = 0x5
+	suite.cpu.registers.R13 = 0x15
+	suite.cpu.registers.R15 = 0x20
+	suite.cpu.registers.R13SupervisorMode = 0x25
+	suite.cpu.registers.R13InterruptMode = 0x30
+	suite.cpu.registers.Cpsr = 0x35
 }
 
 func TestArm7TestSuite(t *testing.T) {
@@ -25,27 +26,58 @@ func TestArm7TestSuite(t *testing.T) {
 }
 
 func (suite *Arm7TestSuite) TestRegisterResetWhenNotBootingFromBIOS() {
-	suite.registers.reset(false)
+	suite.cpu.registers.reset(false)
 
-	assert.Equal(suite.T(), uint32(0x0), suite.registers.r0)
-	assert.Equal(suite.T(), uint32(0x03007F00), suite.registers.r13)
-	assert.Equal(suite.T(), uint32(0x8000000), suite.registers.r15)
-	assert.Equal(suite.T(), uint32(0x03007FE0), suite.registers.r13SupervisorMode)
-	assert.Equal(suite.T(), uint32(0x03007FA0), suite.registers.r13InterruptMode)
-	assert.Equal(suite.T(), uint32(0x5F), suite.registers.cpsr)
+	assert.Equal(suite.T(), uint32(0x0), suite.cpu.registers.R0)
+	assert.Equal(suite.T(), uint32(0x03007F00), suite.cpu.registers.R13)
+	assert.Equal(suite.T(), uint32(0x8000000), suite.cpu.registers.R15)
+	assert.Equal(suite.T(), uint32(0x03007FE0), suite.cpu.registers.R13SupervisorMode)
+	assert.Equal(suite.T(), uint32(0x03007FA0), suite.cpu.registers.R13InterruptMode)
+	assert.Equal(suite.T(), uint32(0x5F), suite.cpu.registers.Cpsr)
 }
 
 func (suite *Arm7TestSuite) TestRegisterResetWhenBootingFromBIOS() {
-	suite.registers.reset(true)
+	suite.cpu.registers.reset(true)
 
-	assert.Equal(suite.T(), uint32(0x0), suite.registers.r0)
-	assert.Equal(suite.T(), uint32(0x0), suite.registers.r13)
-	assert.Equal(suite.T(), uint32(0x0), suite.registers.r15)
-	assert.Equal(suite.T(), uint32(0x0), suite.registers.r13SupervisorMode)
-	assert.Equal(suite.T(), uint32(0x0), suite.registers.r13InterruptMode)
-	assert.Equal(suite.T(), uint32(0xD3), suite.registers.cpsr)
+	assert.Equal(suite.T(), uint32(0x0), suite.cpu.registers.R0)
+	assert.Equal(suite.T(), uint32(0x0), suite.cpu.registers.R13)
+	assert.Equal(suite.T(), uint32(0x0), suite.cpu.registers.R15)
+	assert.Equal(suite.T(), uint32(0x0), suite.cpu.registers.R13SupervisorMode)
+	assert.Equal(suite.T(), uint32(0x0), suite.cpu.registers.R13InterruptMode)
+	assert.Equal(suite.T(), uint32(0xD3), suite.cpu.registers.Cpsr)
 }
 
 func (suite *Arm7TestSuite) TestGetGeneralPurposeRegister() {
-	assert.Equal(suite.T(), uint32(0x5), suite.registers.getRegister(0x0))
+	assert.Equal(suite.T(), uint32(0x5), suite.cpu.getRegister(0))
+}
+
+func (suite *Arm7TestSuite) TestGetGeneralPurposeRegisterWhenChangingCpuModes() {
+	suite.cpu.cpuMode = SYS
+	assert.Equal(suite.T(), uint32(0x15), suite.cpu.getRegister(13))
+
+	suite.cpu.cpuMode = SVC
+	assert.Equal(suite.T(), uint32(0x25), suite.cpu.getRegister(13))
+
+	suite.cpu.cpuMode = IRQ
+	assert.Equal(suite.T(), uint32(0x30), suite.cpu.getRegister(13))
+}
+
+func (suite *Arm7TestSuite) TestSetGeneralPurposeRegister() {
+	suite.cpu.setRegister(0, 0x66)
+	assert.Equal(suite.T(), uint32(0x66), suite.cpu.getRegister(0))
+}
+
+func (suite *Arm7TestSuite) TestSetGeneralPurposeRegisterWhenChangingCpuModes() {
+	suite.cpu.cpuMode = SYS
+	suite.cpu.setRegister(13, 0x66)
+
+	suite.cpu.cpuMode = SVC
+	suite.cpu.setRegister(13, 0x77)
+
+	suite.cpu.cpuMode = IRQ
+	suite.cpu.setRegister(13, 0x88)
+
+	assert.Equal(suite.T(), uint32(0x66), suite.cpu.registers.R13)
+	assert.Equal(suite.T(), uint32(0x77), suite.cpu.registers.R13SupervisorMode)
+	assert.Equal(suite.T(), uint32(0x88), suite.cpu.registers.R13InterruptMode)
 }
