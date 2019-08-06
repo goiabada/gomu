06a6b65fae6f5c3013dce93be678b4db2cb8248e
Nikolas Vago Serafini <nikolas.serafini@zrp.com.br>
2019-08-06 13:32:14 -0300

    Added CPU struct, changed get and set registers functions

--- pkg/arm7/arm7.go
+++ pkg/arm7/arm7.go
@@ -1,10 +1,15 @@
 package arm7
 
 import (
-	"fmt"
 	"log"
-	"reflect"
 )
+
+// CPU defines the processor with all relevant info and registers
+type CPU struct {
+	cpuMode         int8
+	instructionMode int8
+	registers       RegisterSet
+}
 
 // RegisterSet defines virtual CPU registers
 type RegisterSet struct {
@@ -63,6 +68,23 @@ type RegisterSet struct {
 	spsrUndefinedMode uint32
 }
 
+// Constants for defining the CPU modes
+const (
+	USR int8 = iota
+	SYS
+	FIQ
+	SVC
+	ABT
+	IRQ
+	UND
+)
+
+// Constants for defining the current ARM instruction mode
+const (
+	ARM int8 = iota
+	THUMB
+)
+
 func (registers *RegisterSet) reset(usingBIOS bool) {
 	registers.r0, registers.r1, registers.r2, registers.r3, registers.r4, registers.r5 = 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
 	registers.r6, registers.r7, registers.r8, registers.r9, registers.r10 = 0x0, 0x0, 0x0, 0x0, 0x0
@@ -93,52 +115,170 @@ func (registers *RegisterSet) reset(usingBIOS bool) {
 
 }
 
-func (registers *RegisterSet) getRegister(register uint32) uint32 {
-	registerName := fmt.Sprintf("r%d", register)
-	registerValue := reflect.Indirect(reflect.ValueOf(registers)).FieldByName(registerName)
-	// This check is used to avoid "cannot return value obtained from unexported field or method" panic
-	if registerValue.CanInterface() {
-		return registerValue.Interface().(uint32)
+func (cpu *CPU) getRegister(register uint32) uint32 {
+	switch register {
+	case 0:
+		return cpu.registers.r0
+	case 1:
+		return cpu.registers.r1
+	case 2:
+		return cpu.registers.r2
+	case 3:
+		return cpu.registers.r3
+	case 4:
+		return cpu.registers.r4
+	case 5:
+		return cpu.registers.r5
+	case 6:
+		return cpu.registers.r6
+	case 7:
+		return cpu.registers.r7
+	case 8:
+		if cpu.cpuMode == FIQ {
+			return cpu.registers.r8FIQMode
+		}
+		return cpu.registers.r8
+	case 9:
+		if cpu.cpuMode == FIQ {
+			return cpu.registers.r9FIQMode
+		}
+		return cpu.registers.r9
+	case 10:
+		if cpu.cpuMode == FIQ {
+			return cpu.registers.r10FIQMode
+		}
+		return cpu.registers.r10
+	case 11:
+		if cpu.cpuMode == FIQ {
+			return cpu.registers.r11FIQMode
+		}
+		return cpu.registers.r11
+	case 12:
+		if cpu.cpuMode == FIQ {
+			return cpu.registers.r12FIQMode
+		}
+		return cpu.registers.r12
+	case 13:
+		switch cpu.cpuMode {
+		case SYS:
+			return cpu.registers.r13
+		case FIQ:
+			return cpu.registers.r13FIQMode
+		case SVC:
+			return cpu.registers.r13SupervisorMode
+		case ABT:
+			return cpu.registers.r13AbortMode
+		case IRQ:
+			return cpu.registers.r13InterruptMode
+		case UND:
+			return cpu.registers.r13UndefinedMode
+		}
+	case 14:
+		switch cpu.cpuMode {
+		case SYS:
+			return cpu.registers.r14
+		case FIQ:
+			return cpu.registers.r14FIQMode
+		case SVC:
+			return cpu.registers.r14SupervisorMode
+		case ABT:
+			return cpu.registers.r14AbortMode
+		case IRQ:
+			return cpu.registers.r14InterruptMode
+		case UND:
+			return cpu.registers.r14UndefinedMode
+		}
+	case 15:
+		return cpu.registers.r15
+	default:
+		log.Println("You're are trying to access a non-existent register!")
 	}
+
 	return 0x0
 }
 
-func (registers *RegisterSet) setRegister(register uint32, value uint32) {
+func (cpu *CPU) setRegister(register uint32, value uint32) {
 	switch register {
 	case 0:
-		registers.r0 = value
+		cpu.registers.r0 = value
 	case 1:
-		registers.r1 = value
+		cpu.registers.r1 = value
 	case 2:
-		registers.r2 = value
+		cpu.registers.r2 = value
 	case 3:
-		registers.r3 = value
+		cpu.registers.r3 = value
 	case 4:
-		registers.r4 = value
+		cpu.registers.r4 = value
 	case 5:
-		registers.r5 = value
+		cpu.registers.r5 = value
 	case 6:
-		registers.r6 = value
+		cpu.registers.r6 = value
 	case 7:
-		registers.r7 = value
+		cpu.registers.r7 = value
 	case 8:
-		registers.r8 = value
+		if cpu.cpuMode == FIQ {
+			cpu.registers.r8FIQMode = value
+		} else {
+			cpu.registers.r8 = value
+		}
 	case 9:
-		registers.r9 = value
+		if cpu.cpuMode == FIQ {
+			cpu.registers.r9FIQMode = value
+		} else {
+			cpu.registers.r9 = value
+		}
 	case 10:
-		registers.r10 = value
+		if cpu.cpuMode == FIQ {
+			cpu.registers.r10FIQMode = value
+		} else {
+			cpu.registers.r10 = value
+		}
 	case 11:
-		registers.r11 = value
+		if cpu.cpuMode == FIQ {
+			cpu.registers.r11FIQMode = value
+		} else {
+			cpu.registers.r11 = value
+		}
 	case 12:
-		registers.r12 = value
+		if cpu.cpuMode == FIQ {
+			cpu.registers.r12FIQMode = value
+		} else {
+			cpu.registers.r12 = value
+		}
 	case 13:
-		registers.r13 = value
+		switch cpu.cpuMode {
+		case SYS:
+			cpu.registers.r13 = value
+		case FIQ:
+			cpu.registers.r13FIQMode = value
+		case SVC:
+			cpu.registers.r13SupervisorMode = value
+		case ABT:
+			cpu.registers.r13AbortMode = value
+		case IRQ:
+			cpu.registers.r13InterruptMode = value
+		case UND:
+			cpu.registers.r13UndefinedMode = value
+		}
 	case 14:
-		registers.r14 = value
+		switch cpu.cpuMode {
+		case SYS:
+			cpu.registers.r14 = value
+		case FIQ:
+			cpu.registers.r14FIQMode = value
+		case SVC:
+			cpu.registers.r14SupervisorMode = value
+		case ABT:
+			cpu.registers.r14AbortMode = value
+		case IRQ:
+			cpu.registers.r14InterruptMode = value
+		case UND:
+			cpu.registers.r14UndefinedMode = value
+		}
 	case 15:
-		registers.r15 = value
+		cpu.registers.r15 = value
 	default:
-		log.Println("You're are trying to overwrite a non-existent register")
+		log.Println("You're are trying to overwrite a non-existent register!")
 	}
 }
 
